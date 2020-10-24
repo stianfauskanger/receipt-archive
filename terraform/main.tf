0a7b14a582bb0944e19dce8ce825fceacad05223
Stian Fauskanger <stian@fauskanger.net>
2020-10-24 20:35:31 +0200

    Test terraform from CircleCi

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,27 @@
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.70"
+      version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "receipt-archive-src"
+    key            = "terraform/state"
+    dynamodb_table = "terraform-remote-state"
+    region         = "eu-north-1"
+  }
+
 }
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-2"
+  region  = "eu-north-1"
 }
+
+variable region { default = "eu-north-1" }
+variable availability_zone_a { default = "eu-north-1a" }
+variable availability_zone_b { default = "eu-north-1b" }
+variable availability_zone_c { default = "eu-north-1c" }
---
+++ terraform/vpc.tf
@@ -0,0 +1,120 @@
+resource "aws_vpc" "main" {
+  cidr_block         = "10.0.0.0/16"
+  instance_tenancy   = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC for receipt-archive"
+    receipt-archive = 1
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Internet Gateway"
+    receipt-archive = 1
+  }
+}
+
+resource "aws_subnet" "public_subnet_az_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/20"
+  availability_zone = var.availability_zone_a
+
+  tags = {
+    Name = "Public Subnet"
+    receipt-archive = 1
+  }
+}
+
+resource "aws_subnet" "public_subnet_az_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = var.availability_zone_b
+
+  tags = {
+    Name = "Public Subnet"
+    receipt-archive = 1
+  }
+}
+
+resource "aws_subnet" "public_subnet_az_c" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.32.0/20"
+  availability_zone = var.availability_zone_c
+
+  tags = {
+    Name = "Public Subnet"
+    receipt-archive = 1
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "VPC Route Table"
+    receipt-archive = 1
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+//resource "aws_subnet" "private_subnet_az_a" {
+//  vpc_id     = aws_vpc.main.id
+//  cidr_block = "10.0.208.0/20"
+//  availability_zone = var.availability_zone_a
+//
+//  tags = {
+//    Name = "Private Subnet"
+//    receipt-archive = 1
+//  }
+//}
+//
+//resource "aws_subnet" "private_subnet_az_b" {
+//  vpc_id     = aws_vpc.main.id
+//  cidr_block = "10.0.224.0/20"
+//  availability_zone = var.availability_zone_b
+//
+//  tags = {
+//    Name = "Private Subnet"
+//    receipt-archive = 1
+//  }
+//}
+//
+//resource "aws_subnet" "private_subnet_az_c" {
+//  vpc_id     = aws_vpc.main.id
+//  cidr_block = "10.0.240.0/20"
+//  availability_zone = var.availability_zone_c
+//
+//  tags = {
+//    Name = "Private Subnet"
+//    receipt-archive = 1
+//  }
+//}
+//
+//resource "aws_route_table_association" "my_vpc_us_east_1a_public" {
+//  subnet_id = aws_subnet.public_subnet_az_a.id
+//  route_table_id = aws_route_table.main.id
+//}
+//
+//resource "aws_route_table_association" "my_vpc_us_east_1b_public" {
+//  subnet_id = aws_subnet.public_subnet_az_b.id
+//  route_table_id = aws_route_table.main.id
+//}
+//
+//resource "aws_route_table_association" "my_vpc_us_east_1c_public" {
+//  subnet_id = aws_subnet.public_subnet_az_c.id
+//  route_table_id = aws_route_table.main.id
+//}
