e1b6f080a991804c534e1bfeb917131169080bce
Stian Fauskanger <stian@fauskanger.net>
2020-10-24 20:48:12 +0200

    2nd test of terraform from CircleCi

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name            = "Internet Gateway"
+    Name            = "Internet Gateway for receipt-archive"
     receipt-archive = 1
   }
 }
